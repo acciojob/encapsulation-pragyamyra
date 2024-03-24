9134451add51b95f5768e3e8f328fd94451cbb45
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-24 22:34:15 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      obj.setName("Pragya");
+      System.out.println(obj.getName());
+      //Error java: name has private access in com.driver.RWOnly
+
+   }
 }
